8a60a2bd061e63d580c4d8b7c13454a7bd6e2449
Rahul Prasad <prasad_rahul@yahoo.co.in>
2017-03-26 20:00:29 +0530

    Added node project and basic templates

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # nodePanel
 Node.js based sample application for Server Monitoring
+
+## Creating a web app using Node.js and MySQL.
+
+
+## Installation
+1) Download zip or Clone the above repository.
+```
+git clone https://github.com/prasadrahul/nodePanel/
+```
+
+2) Navigate to the project repository and install the dependencies.
+```
+cd nodePanel/
+npm install
+```
+
+3) Point your browser to `http://localhost:8000` and you should have the app running.
---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+app.use('/style',  express.static(__dirname + '/style'));
+app.use('/script',  express.static(__dirname + '/script'));
+
+app.get('/',function(req,res){
+	res.sendFile('home.html',{'root': __dirname + '/templates'});
+})
+
+app.get('/SignInPage',function(req,res){
+	res.sendFile('signin.html',{'root': __dirname + '/templates'});
+})
+
+app.get('/SignUpPage',function(req,res){
+  res.sendFile('signup.html',{'root':__dirname + '/templates'})
+})
+
+app.listen(8000,function(){
+    console.log('Node server running @ http://localhost:8000')
+});
